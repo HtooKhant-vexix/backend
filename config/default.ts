c36fbb30d26859674ac026bd033083eb851e2fc5
det-frontend <digitalengineeringtech.frontend@gmail.com>
2024-01-22 09:28:06 +0630

    final

--- config/default.ts
+++ config/default.ts
@@ -7,9 +7,9 @@ export default {
   secretKey: "suuhh",
   page_limit: 50,
   //mqttUrl: "ws://127.0.0.1:9001",
-  mqttUrl: "ws://192.168.0.100:9001",
   // mqttUrl: "mqtt://192.168.0.10:1883",
   //  mqttUrl: "mqtt://192.168.0.100:1883",
+  mqttUrl: "ws://192.168.0.100:9001",
 
   mqttUserName: "detpos",
   mqttPassword: "asdffdsa",
